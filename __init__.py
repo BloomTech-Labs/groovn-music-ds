550431de107ec5fb78fe76943895e6a846d7933b
ExtraJP2014 <extrajp2014@gmail.com>
2019-10-25 19:43:34 -0400

    update api web server with baseline dependencies

---
+++ __init__.py
@@ -0,0 +1,21 @@
+"""
+Data science Flask API endpoint.
+"""
+
+from flask import Flask, jsonify
+import os
+import random
+import numpy as np
+import psycopg2 as pg
+
+app = Flask(__name__)
+
+@app.route("/")
+def test():
+    """
+    A test function
+    """
+    return "Groovn Music API active!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
